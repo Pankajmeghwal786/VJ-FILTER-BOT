9d5f8f1f7d0934c010e7eba54b34ef14ef9feeea
Pankajmeghwal786 <166579228+Pankajmeghwal786@users.noreply.github.com>
2024-06-18 11:25:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,7 +69,7 @@ OWNER_USERNAME = environ.get('OWNER_USERNAME', 'kingvj01') # owner username with
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/pankaj75')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+y41v4I1V4zs3MDNl')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Linkl')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/pankaj75') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'pankaj75') # Support Chat Link Without https:// or @
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
